b7d215cbf5b92bf21faf0b39863bac81d299d8ae
ImageFaded <33571471+ImageFaded@users.noreply.github.com>
2019-06-07 14:42:28 +0100

    Added in Textbox

--- AlbumOrganisation/AlbumMenu.cs
+++ AlbumOrganisation/AlbumMenu.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Program for keeping track of albums within collection 
+ */
+
+//Including appropriate libraries
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,19 +15,42 @@ using System.Windows.Forms;
 
 namespace AlbumOrganisation
 {
+    //Form to display information relating to albums/artists
     public partial class AlbumMenu : Form
     {
+
+        //Initialises form for album 
         public AlbumMenu()
         {
             InitializeComponent();
-            Settings();
+            MenuDisplaySettings();
+            LoadItems();
         }
 
-        private void Settings()
+        //Sets the size of the album menu form
+        private void MenuDisplaySettings()
         {
             Size = new Size(600, 600);
-            MinimizeBox = false;
+            MaximizeBox = false;
             FormBorderStyle = FormBorderStyle.FixedDialog;
+        }
+
+        //Loads items that will dynamically be displayed onto the screen
+        private void LoadItems()
+        {
+            LoadTextBox();
+        }
+
+        //Settings to load a textbox to the screen which shall display information
+        private void LoadTextBox()
+        {
+            TextBox textList = new TextBox();
+            textList.SetBounds(280, 20, 300, 530);
+            textList.ScrollBars = ScrollBars.Vertical;
+            textList.BackColor = Color.White;
+            textList.ReadOnly = true;
+            textList.Multiline = true;
+            Controls.Add(textList);
         }
 
         private void AlbumMenu_Load(object sender, EventArgs e)
