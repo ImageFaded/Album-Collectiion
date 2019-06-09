5ebbb6bfc0d51105468ad281e9a7f9a7f9f06966
ImageFaded <33571471+ImageFaded@users.noreply.github.com>
2019-06-09 15:18:24 +0100

    Created Sorting Class for main functionality to eventually be added to

---
+++ AlbumOrganisation/AlbumSorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumOrganisation
+{
+    class AlbumSorting
+    {
+        private void SearchArtist()
+        {
+
+        }
+
+        private void SearchAlbum()
+        {
+
+        }
+
+        private void AddArtist()
+        {
+
+        }
+
+        private void RemoveArtist()
+        {
+
+        }
+    }
+}
