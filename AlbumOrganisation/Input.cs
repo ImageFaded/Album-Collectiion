9d608e615443132908bdbf295bb09c062e72d4c0
ImageFaded <33571471+ImageFaded@users.noreply.github.com>
2019-06-17 15:05:05 +0100

    Created function to add album text to file

--- AlbumOrganisation/Input.cs
+++ AlbumOrganisation/Input.cs
@@ -12,28 +12,28 @@ namespace AlbumOrganisation
 {
     public partial class Input : Form
     {
-        public Input(int appearance, AlbumMenu status)
+        public Input(int appearance, AlbumMenu status, string pathToList)
         {
             InitializeComponent();
-            LayoutBox(appearance,status);
+            LayoutBox(appearance,status, pathToList);
         }
 
-        private void LayoutBox(int numcase, AlbumMenu status)
+        private void LayoutBox(int numcase, AlbumMenu status, string pathToList)
         {
             Size = new Size(300, 300);
             switch (numcase)
             {
                 case 0:
-                    SearchArtistLayout(status);
+                    SearchArtistLayout(status,pathToList);
                     break;
                 case 1:
-                    SearchAlbumLayout(status);
+                    SearchAlbumLayout(status, pathToList);
                     break;
                 case 2:
-                    AddAlbumLayout(status);
+                    AddAlbumLayout(status, pathToList);
                     break;
                 case 3:
-                    RemoveAlbumLayout(status);
+                    RemoveAlbumLayout(status, pathToList);
                     break;
                 default:
                     //Nothing so far!
@@ -41,49 +41,43 @@ namespace AlbumOrganisation
             }
         }
 
-        private void SearchArtistLayout(AlbumMenu status)
+        private void SearchArtistLayout(AlbumMenu status, string pathToList)
         {
-            Text = "Search Artist";
-            Button button = new Button();
-            button.SetBounds(90, 180, 100, 40);
-            button.Text = "Search Artist";
-            button.Click += (sender,e) => React(status);
-            Controls.Add(button);
-
             TextBox artistInput = new TextBox();
             artistInput.SetBounds(65, 100, 150, 20);
             //artistInput.Multiline = true;
             artistInput.Tag = "ArtistInput";
             Controls.Add(artistInput);
+
+            Text = "Search Artist";
+            Button button = new Button();
+            button.SetBounds(90, 180, 100, 40);
+            button.Text = "Search Artist";
+            button.Click += (sender, e) => React(status, artistInput, null, 0, pathToList);
+            Controls.Add(button);
+
             MessageBox.Show("Search Artist!");       
         }
 
-        private void SearchAlbumLayout(AlbumMenu status)
+        private void SearchAlbumLayout(AlbumMenu status, string pathToList)
         {
-            Text = "Search Album";
-            Button button = new Button();
-            button.SetBounds(90, 180, 100, 40);
-            button.Text = "Search Album";
-            button.Click += (sender, e) => React(status);
-            Controls.Add(button);
-
             TextBox albumInput = new TextBox();
             albumInput.SetBounds(65, 100, 150, 20);
             //artistInput.Multiline = true;
             albumInput.Tag = "AlbumInput";
-            Controls.Add(button);
-            MessageBox.Show("Search Album!");
-        }
+            Controls.Add(albumInput);
 
-        private void AddAlbumLayout(AlbumMenu status)
-        {
             Text = "Search Album";
             Button button = new Button();
             button.SetBounds(90, 180, 100, 40);
             button.Text = "Search Album";
-            button.Click += (sender, e) => React(status);
+            button.Click += (sender, e) => React(status, albumInput, null, 0, pathToList);
             Controls.Add(button);
+            MessageBox.Show("Search Album!");
+        }
 
+        private void AddAlbumLayout(AlbumMenu status, string pathToList)
+        {
             TextBox[] addAlbumInput = new TextBox[2];
             for(int i = 0; i < 2; i++)
             {
@@ -92,18 +86,19 @@ namespace AlbumOrganisation
                 addAlbumInput[i].Tag = "AddAlbumInput" + i;
                 Controls.Add(addAlbumInput[i]);
             }
-            MessageBox.Show("Add Album!");
-        }
 
-        private void RemoveAlbumLayout(AlbumMenu status)
-        {
-            Text = "Remove Album";
+            Text = "Add Album";
             Button button = new Button();
             button.SetBounds(90, 180, 100, 40);
-            button.Text = "Remove Album";
-            button.Click += (sender, e) => React(status);
+            button.Text = "Add Album";
+            button.Click += (sender, e) => React(status,addAlbumInput[0],addAlbumInput[1],1,pathToList);
             Controls.Add(button);
 
+            //MessageBox.Show("Add Album!");
+        }
+
+        private void RemoveAlbumLayout(AlbumMenu status, string pathToList)
+        {
             TextBox[] removeAlbumInput = new TextBox[2];
             for (int i = 0; i < 2; i++)
             {
@@ -112,14 +107,42 @@ namespace AlbumOrganisation
                 removeAlbumInput[i].Tag = "RemoveAlbumInput" + i;
                 Controls.Add(removeAlbumInput[i]);
             }
+
+            Text = "Remove Album";
+            Button button = new Button();
+            button.SetBounds(90, 180, 100, 40);
+            button.Text = "Remove Album";
+            button.Click += (sender, e) => React(status,removeAlbumInput[0],removeAlbumInput[1],1,pathToList);
+            Controls.Add(button);
+
             MessageBox.Show("Remove Album!");
         }
 
-        private bool React(AlbumMenu status)
+        private bool React(AlbumMenu status, TextBox inputOne, TextBox inputTwo, int type, string pathToList)
         {
-            Close();
-            status.TopMost = true;
-            return status.Enabled = true;
+            switch (type)
+            {
+                case 0:
+                    break;
+                case 1:
+                    //Add Album
+                    if(inputOne.Text.ToString() == "" || inputTwo.Text.ToString() == "")
+                    {
+                        MessageBox.Show("Incomplete Parameter!");                      
+                        break;
+                    }
+                    else
+                    {
+                        //Do Thing
+                        AlbumMenu.SaveTextAccess(inputOne.Text.ToString(), inputTwo.Text.ToString(), pathToList);
+                        Close();
+                        status.TopMost = true;
+                        return status.Enabled = true;
+                    }
+                default:
+                    break;
+            }
+            return false;
         }
     }
 }
